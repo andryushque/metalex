10e8e1dcc01884e350dcfe905fa47865d1060502
andryushque <andrew.ustinov23@gmail.com>
2020-09-05 12:07:45 +0300

    Create: fixed header, anchors

--- js/main.js
+++ js/main.js
@@ -5,4 +5,24 @@ $(document).ready(function () {
     mmenuButton.toggleClass("active");
     $(".nav").toggleClass("nav--hidden");
   });
+
+  /*=== Fixed header ===*/
+  let navOffsetTop = $(".header").height() + 50;
+
+  function fixNavbarOnTop() {
+    if ($(".header").length) {
+      $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if (scroll >= navOffsetTop) {
+          $(".header").addClass("header--fixed");
+          $(".header").removeClass("header-bg");
+        } else {
+          $(".header").removeClass("header--fixed");
+          $(".header").addClass("header-bg");
+        }
+      });
+    }
+  }
+
+  fixNavbarOnTop();
 });
