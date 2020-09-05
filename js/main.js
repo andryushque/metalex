b3480593f4c6ad435ca429e889e99e5ec88f691a
andryushque <andrew.ustinov23@gmail.com>
2020-09-05 14:08:43 +0300

    Create: scrolling to anchors

--- js/main.js
+++ js/main.js
@@ -7,17 +7,17 @@ $(document).ready(function () {
   });
 
   /*=== Fixed header ===*/
-  var headerOffsetTop = $(".header").height() + 90;
+  var headerOffsetTop = $(".header").height() + 200;
   function fixHeaderOnTop() {
     if ($(".header").length) {
       $(window).scroll(function () {
         let scroll = $(window).scrollTop();
         if (scroll >= headerOffsetTop) {
           $(".header").addClass("header--fixed");
-          $(".header").removeClass("header-bg");
+          $(".header").removeClass("header--bg");
         } else {
           $(".header").removeClass("header--fixed");
-          $(".header").addClass("header-bg");
+          $(".header").addClass("header--bg");
         }
       });
     }
@@ -36,6 +36,53 @@ $(document).ready(function () {
 
   btn.on("click", function (e) {
     e.preventDefault();
-    $("html, body").animate({ scrollTop: 0 }, "300");
+    $("html, body").animate({ scrollTop: 0 }, "1000");
   });
+
+  /*=== Smooth Scrolling to Anchor ===*/
+  // Select all links with hashes and remove not anchor links
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // Hide mobile menu on scrolling
+      $(".mmenu").toggleClass("active");
+      $(".nav").toggleClass("nav--hidden");
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
 });
